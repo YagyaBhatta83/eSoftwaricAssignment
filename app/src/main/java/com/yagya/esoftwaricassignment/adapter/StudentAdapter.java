2e45c52e04b7181897d5ae41ae016caadfab6669
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-27 00:11:18 +0545

    added student shown in home.

---
+++ app/src/main/java/com/yagya/esoftwaricassignment/adapter/StudentAdapter.java
@@ -0,0 +1,94 @@
+package com.yagya.esoftwaricassignment.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yagya.esoftwaricassignment.R;
+import com.yagya.esoftwaricassignment.Student;
+
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+    Context Context;
+    List<Student> studentList;
+
+    public StudentAdapter(Context context, List<Student> studentList){
+        this.Context = Context;
+        this.studentList= studentList;
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_student, parent, false);
+        return new StudentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHolder holder, final int position) {
+        final Student student = studentList.get(position);
+        holder.tvName.setText(student.getName());
+        holder.tvAge.setText(student.getAge());
+        holder.tvAddress.setText(student.getAddress());
+        holder.tvGender.setText(student.getGender());
+
+        String gender = student.getGender();
+
+        if (gender=="Male") {
+            holder.ivProfile.setImageResource(R.drawable.male);
+        }
+        else if(gender=="Female"){
+            holder.ivProfile.setImageResource(R.drawable.female);
+        }
+        else {
+            holder.ivProfile.setImageResource(R.drawable.other);
+        }
+
+        holder.ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Context,"hi"+student.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Student st = studentList.get(position);
+                studentList.remove(position);
+                notifyItemRemoved(position);
+                Toast.makeText(Context,"Removed:"+st.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+    public class StudentViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivProfile;
+        TextView tvName, tvAge, tvAddress, tvGender;
+        Button btnDelete;
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfile = itemView.findViewById(R.id.imageView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvAge = itemView.findViewById(R.id.tvAge);
+            tvAddress = itemView.findViewById(R.id.tvAddress);
+            tvGender = itemView.findViewById(R.id.tvGender);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
+        }
+    }
+}
--- app/src/main/java/com/yagya/esoftwaricassignment/fragment/HomeFragment.java
+++ app/src/main/java/com/yagya/esoftwaricassignment/fragment/HomeFragment.java
@@ -4,17 +4,25 @@ package com.yagya.esoftwaricassignment.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.yagya.esoftwaricassignment.R;
+import com.yagya.esoftwaricassignment.Student;
+import com.yagya.esoftwaricassignment.adapter.StudentAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
 
 
     public HomeFragment() {
@@ -26,7 +34,18 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        List<Student> studentList = new ArrayList<>();
+
+        studentList.add(new Student("Rupesh Dhakal", "22", "Balaju", "Male"));
+        studentList.add(new Student("Rita Sharma", "25", "Baneshwor", "Female"));
+        StudentAdapter studentAdapter = new StudentAdapter(getContext(), studentList);
+        recyclerView.setAdapter(studentAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
     }
 
 }
