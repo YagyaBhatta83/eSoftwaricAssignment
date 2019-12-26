17de9ddc6e119741470ba4c031dde86aeb08fe9e
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-26 09:24:40 +0545

    Button navigation dependency added. layout designed of button navigation and code for selected button with its heading.

--- app/src/main/java/com/yagya/esoftwaricassignment/MainActivity.java
+++ app/src/main/java/com/yagya/esoftwaricassignment/MainActivity.java
@@ -1,14 +1,47 @@
 package com.yagya.esoftwaricassignment;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        toolbar.setTitle("Home");
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle("Home");
+                    return true;
+                case R.id.navigation_addstudent:
+                    toolbar.setTitle("Add Student");
+                    return true;
+                case R.id.navigation_aboutus:
+                    toolbar.setTitle("About Us");
+                    return true;
+            }
+            return false;
+        }
+    };
 }
