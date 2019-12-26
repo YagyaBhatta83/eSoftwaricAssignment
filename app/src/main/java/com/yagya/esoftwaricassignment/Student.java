f7e72f316da0e9a8577d08576da03623fd42c412
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-26 23:07:30 +0545

    student added successfully.

---
+++ app/src/main/java/com/yagya/esoftwaricassignment/Student.java
@@ -0,0 +1,47 @@
+package com.yagya.esoftwaricassignment;
+
+public class Student {
+    private String name;
+    private String age;
+    private String address;
+    private String gender;
+
+    public Student(String name, String age, String address, String gender) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
--- app/src/main/java/com/yagya/esoftwaricassignment/fragment/AddStudentFragment.java
+++ app/src/main/java/com/yagya/esoftwaricassignment/fragment/AddStudentFragment.java
@@ -5,16 +5,31 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.yagya.esoftwaricassignment.R;
+import com.yagya.esoftwaricassignment.Student;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddStudentFragment extends Fragment {
+    private EditText etFullName, etAge, etAddress;
+    private Button btnSave;
+    private RadioGroup rgGender;
+    private RadioButton rbMale, rbFemale, rbOther;
+    String name, age, address, gender;
 
 
     public AddStudentFragment() {
@@ -26,7 +41,71 @@ public class AddStudentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_student, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_add_student, container, false);
 
+        etFullName = view.findViewById(R.id.etFullName);
+        etAge = view.findViewById(R.id.etAge);
+        etAddress = view.findViewById(R.id.etAddress);
+        rgGender = view.findViewById(R.id.rgGender);
+        rbMale = view.findViewById(R.id.rbMale);
+        rbFemale = view.findViewById(R.id.rbFemale);
+        rbOther = view.findViewById(R.id.rbOther);
+        btnSave = view.findViewById(R.id.btnSave);
+
+        rgGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rbMale) {
+                    gender = "Male";
+                }
+                if (checkedId == R.id.rbFemale) {
+                    gender = "Female";
+                }
+                if (checkedId == R.id.rbOther) {
+                    gender = "Others";
+                }
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = etFullName.getText().toString();
+                address = etAddress.getText().toString();
+                age = etAge.getText().toString();
+
+                if(TextUtils.isEmpty(name))
+                {
+                    etFullName.setError("Enter a name");
+                    return;
+                }
+                if(TextUtils.isEmpty(age))
+                {
+                    etAge.setError("Enter a age");
+                    return;
+                }
+                if(TextUtils.isEmpty(address))
+                {
+                    etAddress.setError("Enter a Address");
+                    return;
+                }
+                if(TextUtils.isEmpty(gender))
+                {
+                    Toast.makeText(getActivity(), "Select a Gender", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                List<Student> studentList = new ArrayList<>();
+                studentList.add(new Student(name,age,address,gender));
+                Toast.makeText(getActivity(), "Student added", Toast.LENGTH_SHORT).show();
+
+                etFullName.setText("");
+                etAge.setText("");
+                etAddress.setText("");
+                rgGender.clearCheck();
+
+            }
+        });
+        return view;
+    }
 }
