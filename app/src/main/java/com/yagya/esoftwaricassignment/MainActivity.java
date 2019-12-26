eb1cb90b8b916c953644b9d2a4276d65673834b1
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-26 10:28:44 +0545

    add student layout design created and code for load fragment in main activity.

--- app/src/main/java/com/yagya/esoftwaricassignment/MainActivity.java
+++ app/src/main/java/com/yagya/esoftwaricassignment/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.yagya.esoftwaricassignment.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActionBar toolbar;
@@ -20,10 +22,24 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = getSupportActionBar();
 
+        // load the store fragment by default
+
+        toolbar.setTitle("Home");
+        loadFragment(new HomeFragment());
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         toolbar.setTitle("Home");
     }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
